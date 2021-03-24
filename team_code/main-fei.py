b2d947de0b5fd667409b61116980f671e00d881b
greenseven77 <greenseven@gmail.com>
2021-03-24 21:56:35 +0000

    try to change main-fei.py to file id again

--- team_code/main-fei.py
+++ team_code/main-fei.py
@@ -174,4 +174,4 @@ def text_sentiment_line_chart(df_sentiment_score):
 if __name__ == "__main__":
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.2", port=8080, debug=True)
