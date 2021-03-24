3664327c145b8b525b6923f11e8e4fecf0fa84a7
greenseven77 <greenseven@gmail.com>
2021-03-24 16:46:14 +0000

    added the team coding folder for projecct

--- team_code/main-fei.py
+++ team_code/main-fei.py
@@ -120,6 +120,85 @@ def analyze_text_sentiment(text):
     return sentence_sentiment
 
 
+# Module to give 'sentiment' results:
+# take a sentence, run the module, result back:
+# 1. original text; 
+# 2. sentiment score -- between -1 to 1
+# 3. sentiment magnitude -- absolute value of the score, but will agrregate
+def analyze_text_sentiment(text):
+    client = language.LanguageServiceClient()
+    document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
+
+    response = client.analyze_sentiment(document=document)
+
+    sentiment = response.document_sentiment
+    results = dict(
+        text=text,
+        score=f"{sentiment.score:.1%}",
+        magnitude=f"{sentiment.magnitude:.1%}",
+    )
+    
+    # Get sentiment for all sentences in the document
+    sentence_sentiment = []
+    for sentence in response.sentences:
+        item={}
+        item["text"]=sentence.text.content
+        item["sentiment score"]=sentence.sentiment.score
+        item["sentiment magnitude"]=sentence.sentiment.magnitude
+        sentence_sentiment.append(item)
+    
+    return sentence_sentiment
+
+# return results by sentence
+def save_sentiment_to_df (text):
+    df_sentiment=pd.DataFrame(analyze_text_sentiment(text))
+    return df_sentiment
+
+# get text from a file
+def get_text_from_file (file_name):
+    #open text file in read mode
+    text_file = open(file_name, "r")
+    
+    #read whole file to a string
+    data = text_file.read()
+
+    #close file
+    text_file.close()
+
+    return data
+
+#Get basic information
+def get_basic_info(file_name):
+    text = get_text_from_file (file_name)
+    # create the result into dataframe for further info
+    df_text_sentiment_stats = save_sentiment_to_df(text)
+    df_text_sentiment_stats.reset_index(inplace=True)
+    # 1. how many sentense?
+    cnt_sentences = df_text_sentiment_stats.shape[0]
+    print("There are total of ", cnt_sentences, " sentences in the artical.")
+    # 2. The sentiment score of each sentence:
+    #print(df_text_sentiment_stats['sentiment score'])
+    return df_text_sentiment_stats['sentiment score']
+
+# visualization (code test) 
+def text_sentiment_line_chart(df_sentiment_score):
+    df_sentiment_score.plot(kind='line', y='sentiment score', x=df_sentiment_score.index+1)
+
+    # Build the chart graphic
+    ax = plt.axes()
+    ax.grid()
+    ax.margins(0) # remove default margins (matplotlib verision 2+)
+    ax.axhspan(0, 2.0, facecolor='bisque', alpha=0.5)
+    ax.axhspan(-2.0, 0, facecolor='lightcoral', alpha=0.5)
+
+    # Giving y label using xlabel() method 
+    plt.ylabel("Sentiment Score", fontweight='bold')  
+    # Giving title to the plot
+    plt.title("Sentiment Score by Sentence", fontweight='bold')
+    plt.show()
+    
+    
+
 if __name__ == "__main__":
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
